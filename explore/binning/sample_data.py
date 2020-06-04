32d04e884d5861f260f3f53b71ebf83937454a5e
Beatrix Haddock <beatrixh@gen-uge-submit-p01.hosts.ihme.washington.edu>
2020-06-04 11:24:55 -0700

    added fn to output samples

--- explore/binning/sample_data.py
+++ explore/binning/sample_data.py
@@ -16,3 +16,35 @@ def sample_data(row, age_distribution):
     # draw random specific ages
     return np.random.choice(a=vals, p=probs, size = size)
 
+def generate_single_year_df(input_df, age_distribution):
+    ''' takes in a df with geoid/sex_id/age-bins
+        outputs df with geoid/sex_id/single-year-ages
+    '''
+    # initialize output df with pop_counts == 0
+    df = input_df[['geoid','sex_id']]
+    
+    for i in np.arange(121):
+        var = str(i)
+        df[var] = 0
+
+    df = df.melt(id_vars = ['geoid','sex_id'],
+                 value_vars = [str(i) for i in np.arange(121)],
+                 var_name='age', value_name = 'pop_count')
+
+    df.age = df.age.astype('int64')
+    
+    # fill pop_count
+    for i, row in input_df.iterrows():
+        geoid = row.geoid
+        sex_id = row.sex_id
+        single_years = sample_data(row, age_distribution)
+        for i in single_years:
+            df.loc[(df.age==i) & (df.sex_id==sex_id) & (df.geoid==geoid),'pop_count'] +=1
+    
+    # test
+    assert(input_df.pop_count.sum()==df.pop_count.sum()), 'Total population mismatch'
+    
+    assert(input_df.geoid.nunique()==df.geoid.nunique()), 'Geoid mismatch'
+    
+    return df
+    
